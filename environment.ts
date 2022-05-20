2bfda66738f7efd71c52ee9ec8687279c82a1035
PereAnd <aa.pereza1@uniandes.edu.co>
2022-05-20 09:28:50 -0500

    Initial commit

---
+++ environment.ts
@@ -0,0 +1,8 @@
+export const environment = {
+    urlGhost: "http://localhost:2368/ghost/#/signin",
+    pathScreenshots: 'screenshots/',
+    siteTitle: 'TestSiteElsa',
+    fullName: 'Elsa Pato',
+    email: 'correo@correo.com',
+    pass: 'ElsaTest15+'
+};
